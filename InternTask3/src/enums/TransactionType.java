98743f6be40b6cbf6c2f6cfd4f8ef1ee755abefa
logywaran <150234568+logywaran@users.noreply.github.com>
2026-04-27 11:53:38 +0530

    Delete InternTask3 directory

--- InternTask3/src/enums/TransactionType.java
+++
@@ -1,5 +0,0 @@
-package enums;
-
-public enum TransactionType {
-    ADD, DEPOSIT, WITHDRAW
-}
--- InternTask3/src/exception/CurrencyNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class CurrencyNotFoundException extends RuntimeException {
-    public CurrencyNotFoundException(String message) {
-        super(message);
-    }
-}
